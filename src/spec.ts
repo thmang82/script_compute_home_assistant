4c78e7f1185822bc642525ad99e381760360c59a
ThMang <thmang82@gmail.com>
2023-08-07 22:34:48 +0200

    Adapt to API changes

--- src/spec.ts
+++ src/spec.ts
@@ -7,7 +7,7 @@ export const specification: DataSource.Specification = {
     // ---
     provides: [ "compute", "device_lights", "device_covers" ],
     // ---
-    version:   "0.1.0",
+    version:   "0.2.0",
     // ---
     author_email: "",
     translations: {
